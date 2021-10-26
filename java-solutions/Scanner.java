c1b8f529cd1c413758e508949ce5ae111a898c3a
Chechevatov <chechevatov.r@gmail.com>
2021-10-26 15:38:45 +0300

    Initial commit for HW-6 WsppSortedSecondG

---
+++ java-solutions/Scanner.java
@@ -0,0 +1,272 @@
+import java.io.*;
+import java.nio.CharBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.InputMismatchException;
+import java.util.function.Predicate;
+
+class isNotWhitespace implements Predicate<Character> {
+    @Override
+    public boolean test(Character c) {
+        return !Character.isWhitespace(c);
+    }
+}
+
+class isWordCharacter implements Predicate<Character> {
+    @Override
+    public boolean test(Character c) {
+        return Character.isLetter(c) || Character.getType(c) == Character.DASH_PUNCTUATION || c == '\'';
+    }
+}
+
+class isLineSeparator implements Predicate<Character> {
+    // https://en.wikipedia.org/wiki/Newline#Unicode
+    @Override
+    public boolean test(Character c) {
+        return
+            c == 0x000A || // LF | Line feed
+            c == 0x000B || // VT | Vertical tab
+            c == 0x000C || // FF | Form feed
+            c == 0x000D || // CR | Carriage return
+            c == 0x0085 || // NL | Next line
+            c == 0x2028 || // LS | Line separator
+            c == 0x2029;   // PS | Paragraph separator
+    }
+}
+
+public class Scanner {
+    private final int BUFFER_SIZE = 512;
+    private final int READ_AHEAD_LIMIT = 1 << 21;
+
+    private final Reader reader;
+    private final CharBuffer buf = CharBuffer.allocate(this.BUFFER_SIZE).limit(0);
+    private final CharBuffer bufFrw = CharBuffer.allocate(this.BUFFER_SIZE).limit(0);
+
+    private final Predicate<Character> isNotWhitespace = new isNotWhitespace();
+    private final Predicate<Character> isWordCharacter = new isWordCharacter();
+    private final Predicate<Character> isLineSeparator = new isLineSeparator();
+
+    public int linesSkipped = 0;
+
+    // =================================[ CONSTRUCTORS ]=================================
+
+    public Scanner(File file) throws FileNotFoundException {
+        this.reader = new BufferedReader(
+            new InputStreamReader(
+                new FileInputStream(file),
+                StandardCharsets.UTF_8
+            )
+        );
+    }
+
+    public Scanner(InputStream is) {
+        this.reader = new BufferedReader(new InputStreamReader(is));
+    }
+
+    // ==========================[ BASIC READER INTERACTIONS ]===========================
+
+    public void close() {
+        try {
+            this.reader.close();
+        } catch (IOException e) {
+            System.err.println("There was an IOException when closing scanner");
+        }
+    }
+
+    private void mark() throws IOException {
+        try {
+            this.reader.mark(this.READ_AHEAD_LIMIT);
+        } catch (IOException e) {
+            System.err.println("There was a problem when marking reader: " + e.getMessage());
+            throw e;
+        }
+        int sz = this.buf.limit() - this.buf.position();
+        int ps = this.buf.position();
+        this.bufFrw.clear();
+        this.bufFrw.put(this.buf);
+        this.buf.position(ps);
+        this.bufFrw.rewind();
+        this.bufFrw.limit(sz);
+    }
+
+    private void reset() throws IOException {
+        try {
+            this.reader.reset();
+        } catch (IOException e) {
+            System.err.println("There was a problem when resetting reader: " + e.getMessage());
+            throw e;
+        }
+    }
+
+    private void read(CharBuffer dst) throws IOException {
+        // Reads into the buffer with destroying data already stored
+        dst.clear();
+        dst.limit(Math.max(0, this.reader.read(dst)));
+        dst.rewind();
+    }
+
+    // ===============================[ TOKEN SPLITTING ]================================
+
+    private String nextToken(CharBuffer buf, Predicate<Character> isTokenChar, boolean mustMark) throws IOException {
+        if (mustMark) {
+            this.mark();
+        }
+        try {
+            StringBuilder sb = new StringBuilder();
+            boolean found = false;
+            this.linesSkipped = 0;
+            while (true) {
+                boolean mustExit = false;
+                while (!buf.isEmpty()) {
+                    char cur = buf.get();
+                    if (isTokenChar.test(cur)) {
+                        found = true;
+                        sb.append(cur);
+                    } else if (found) {
+                        buf.position(buf.position() - 1);
+                        mustExit = true;
+                        break;
+                    } else if (this.isLineSeparator.test(cur)) {
+                        this.linesSkipped++;
+                        if (cur == '\r') {
+                            if (buf.isEmpty()) {
+                                this.read(buf);
+                            }
+                            if (!buf.isEmpty()) {
+                                if (buf.get() != '\n') {
+                                    // Считать символ и увеличить позицию
+                                    // Если \n - оставить как есть
+                                    // Иначе вернуть позицию обратно
+                                    buf.position(buf.position() - 1);
+                                }
+                            }
+                        }
+                    }
+                }
+                if (mustExit) {
+                    break;
+                }
+                this.read(buf);
+                if (buf.isEmpty()) {
+                    break;
+                }
+            }
+            return sb.toString();
+        } catch (IOException e) {
+            throw e;
+        } finally {
+            if (mustMark) {
+                this.reset();
+            }
+        }
+        
+    }
+
+    // ==================================[ NUMERICS ]====================================
+
+    public boolean hasNextInt() throws IOException {
+        try {
+            Integer.parseInt(this.nextToken(this.bufFrw, this.isNotWhitespace, true));
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public int nextInt() throws InputMismatchException, IOException {
+        try {
+            return Integer.parseInt(this.nextToken(this.buf, this.isNotWhitespace, false));
+        } catch (NumberFormatException e) {
+            throw new InputMismatchException();
+        }
+    }
+
+    public boolean hasNextHex() throws IOException {
+        String token = this.nextToken(this.bufFrw, this.isNotWhitespace, true);
+        if (token.startsWith("0x") || token.startsWith("0X")) {
+            try {
+                Integer.parseUnsignedInt(token.substring(2), 16);
+                return true;
+            } catch (NumberFormatException ignored) {}
+        }
+        return false;
+    }
+
+    public int nextHex() throws InputMismatchException, IOException {
+        String token = this.nextToken(this.buf, this.isNotWhitespace, false);
+        if (token.startsWith("0x") || token.startsWith("0X")) {
+            try {
+                return Integer.parseUnsignedInt(token.substring(2), 16);
+            } catch (NumberFormatException ignored) {}
+        }
+        throw new InputMismatchException();
+    }
+
+    private String literalToDigital(String s) throws NumberFormatException {
+        if (s.isEmpty()) {
+            return s;
+        }
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        if (s.charAt(0) == '-') {
+            sb.append('-');
+            i++;
+        }
+        for (/* Using i defined before */; i < s.length(); i++) {
+            char cur = s.charAt(i);
+            if ('a' <= cur && cur < 'a' + 10) {
+                sb.append((char) (cur - 'a' + '0'));
+            } else {
+                throw new NumberFormatException();
+            }
+        }
+        return sb.toString();
+    }
+
+    public boolean hasNextLit() throws IOException {
+        String token = this.nextToken(this.bufFrw, this.isNotWhitespace, true);
+        try {
+            Integer.parseInt(literalToDigital(token));
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public int nextLit() throws InputMismatchException, IOException {
+        String token = this.nextToken(this.buf, this.isNotWhitespace, false);
+        try {
+            return Integer.parseInt(literalToDigital(token));
+        } catch (NumberFormatException e) {
+            throw new InputMismatchException();
+        } 
+    }
+
+    public boolean hasNextNumber() throws IOException {
+        return this.hasNextInt() || this.hasNextHex() || this.hasNextLit();
+    }
+
+    public int nextNumber() throws IOException {
+        if (this.hasNextInt()) {
+            return this.nextInt();
+        } else if (this.hasNextHex()) {
+            return this.nextHex();
+        } else if (this.hasNextLit()) {
+            return this.nextLit();
+        }
+        throw new InputMismatchException();
+    }
+
+    // ===================================[ WORDS ]======================================
+
+    public boolean hasNextWord() throws IOException {
+        return !this.nextToken(this.bufFrw, this.isWordCharacter, true).isEmpty();
+    }
+
+    public String nextWord() throws InputMismatchException, IOException {
+        String token = this.nextToken(this.buf, this.isWordCharacter, false);
+        if (token.isEmpty()) {
+            throw new InputMismatchException();
+        }
+        return token;
+    }
+}
---
+++ java-solutions/WsppSortedSecondG.java
@@ -0,0 +1,132 @@
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
+class IntList {
+    private int[] c;
+    public int length;
+
+    IntList() {
+        c = new int[1];
+        length = 0;
+    }
+
+    IntList(int val) {
+        c = new int[1];
+        c[0] = val;
+        length = 1;
+    }
+
+    public void add(int val) {
+        if (length >= c.length) {
+            c = Arrays.copyOf(c, length * 2);
+        }
+        c[length++] = val;
+    }
+
+    public int get(int pos) {
+        return c[pos];
+    }
+
+    public void shrink() {
+        c = Arrays.copyOf(c, length);
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < length - 1; i += 2) {
+            sb.append(c[i]);
+            sb.append(" ");
+        }
+        if (length % 2 == 0) {
+            sb.append(c[length - 1]);
+        }
+        if (!sb.isEmpty() && sb.charAt(sb.length() - 1) == ' ') {
+            sb.deleteCharAt(sb.length() - 1);
+        }
+        return sb.toString();
+    }
+}
+
+public class WsppSortedSecondG {
+    public static void main(String[] args) {
+        ArrayList<String> words = new ArrayList<String>();
+        Map<String, Map<Integer, IntList>> wordsByLines = new HashMap<String, Map<Integer, IntList>>();
+        Map<String, Integer> wordCounts = new HashMap<String, Integer>();
+
+        Scanner in = null;
+        try {
+            in = new Scanner(new File(args[0]));
+            try {
+                int curLine = 0;
+                int curPos = 0;
+                while (in.hasNextWord()) {
+                    String word = in.nextWord().toLowerCase();
+                    curLine += in.linesSkipped;
+                    curPos++;
+                    IntList line;
+                    Map<Integer, IntList> wordMap = wordsByLines.get(word);
+                    wordCounts.put(word, wordCounts.getOrDefault(word, 0) + 1);
+
+                    if (wordMap == null) {
+                        words.add(word);
+                        line = new IntList(curPos);
+                        wordMap = new TreeMap<Integer, IntList>();
+                        wordMap.put(curLine, line);
+                        wordsByLines.put(word, wordMap);
+                    } else {
+                        line = wordMap.get(curLine);
+                        if (line == null) {
+                            line = new IntList(curPos);
+                            wordMap.put(curLine, line);
+                        } else {
+                            line.add(curPos);
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                System.err.println("IOException happened while Scanner was scanning: " + e.getMessage());
+            } finally {
+                in.close();
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("Input file was not found: " + e.getMessage());
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+        }
+        Collections.sort(words);
+        try {
+            BufferedWriter output = new BufferedWriter(
+                    new OutputStreamWriter(
+                            new FileOutputStream(args[1]),
+                            StandardCharsets.UTF_8
+                    )
+            );
+            try {
+                for (String word : words) {
+                    StringBuilder sb = new StringBuilder();
+                    sb.append(word).append(" ").append(wordCounts.get(word)).append(" ");
+                    for (Map.Entry<Integer, IntList> entry : wordsByLines.get(word).entrySet()) {
+                        String s = entry.getValue().toString();
+                        if (!s.isEmpty()) {
+                            sb.append(s).append(" ");
+                        }
+                    }
+                    if (!sb.isEmpty() && sb.charAt(sb.length() - 1) == ' ') {
+                        sb.deleteCharAt(sb.length() - 1);
+                    }
+                    output.write(sb.toString());
+                    output.write(System.lineSeparator());
+                }
+            } catch (IOException e) {
+                System.err.println("IO Exception happened while writing output file: " + e.getMessage());
+            } finally {
+                output.close();
+            }
+        } catch (IOException e) {
+            System.err.println("IO Exception happened while opening or closing output file: " + e.getMessage());
+        }
+    }
+}
