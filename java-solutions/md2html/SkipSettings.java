2923a111375285a9ff28d709568e3d98af38e7a7
Roman Chechevatov <chechevatov.r@gmail.com>
2021-11-16 16:13:58 +0300

    [ДЗ-9][Мод] Объединил параметры функций в отдельный объект

---
+++ java-solutions/md2html/SkipSettings.java
@@ -0,0 +1,53 @@
+package md2html;
+
+public class SkipSettings {
+    public boolean emphasis;
+    public boolean strong;
+    public boolean strikeout;
+    public boolean code;
+    public boolean ins;
+    public boolean del;
+
+    public SkipSettings() {
+        emphasis = false;
+        strong = false;
+        strikeout = false;
+        code = false;
+        ins = false;
+        del = false;
+    }
+
+    public SkipSettings(SkipSettings copyFrom) {
+        emphasis = copyFrom.emphasis;
+        strong = copyFrom.strong;
+        strikeout = copyFrom.strikeout;
+        code = copyFrom.code;
+        ins = copyFrom.ins;
+        del = copyFrom.del;
+    }
+
+    SkipSettings setKey(String keyToSetTrue) {
+        SkipSettings copy = new SkipSettings(this);
+        switch (keyToSetTrue) {
+            case "emphasis":
+                copy.emphasis = true;
+                break;
+            case "strong":
+                copy.strong = true;
+                break;
+            case "strikeout":
+                copy.strikeout = true;
+                break;
+            case "code":
+                copy.code = true;
+                break;
+            case "ins":
+                copy.ins = true;
+                break;
+            case "del":
+                copy.del = true;
+                break;
+        }
+        return copy;
+    }
+}
